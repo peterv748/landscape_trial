d2fa6063a3af4ec0c00010f91a44d25a673a167c
peterv748 <peter.verhoeve@caiway.nl>
2021-01-25 20:41:14 +0100

    commit 25 January 2021 004

--- landscape.py
+++ landscape.py
@@ -5,36 +5,29 @@ import numpy as np
 from numba import jit_module
 import random
 
-
-
 def compute_average(*args):
     """
     compute the float average of input arguments
     """
-
     return float((sum(args))/len(args))
 
 
-def random_generator():
+def random_generator(seed):
     """
     randon generator to use in diamond square algorithm
     """
+    return random.randint(-seed, seed)
 
-    mu = 5.0
-    sigma = 2.5
-
-    return random.gauss(mu, sigma)
-
-def init_grid(size):
+def init_grid(size, random_seed):
     """
     initialize the diamond square grid
     """
     i = int(size)
     grid = np.zeros([i, i])
-    grid[0,0] = random_generator()
-    grid[(i-1), 0] = random_generator()
-    grid[(0,(i-1) )] = random_generator()
-    grid[((i-1), (i-1))] = random_generator()
+    grid[0,0] = random_generator(random_seed)
+    grid[(i-1), 0] = random_generator(random_seed)
+    grid[(0,(i-1) )] = random_generator(random_seed)
+    grid[((i-1), (i-1))] = random_generator(random_seed)
 
     return grid
 
@@ -117,19 +110,19 @@ def recalculate_positions(midpoint, current_length):
     return nw_position, ne_position, sw_position, se_position
 
 
-def calculate_squarestep_value(grid, midpoint, upper_left,  upper_right, lower_left, lower_right):
+def calculate_squarestep_value(grid, midpoint, upper_left,  upper_right, lower_left, lower_right, current_random_seed):
     """
     calculate the new value for the midpoint in the square step
     """
     new_value = 0.0
-    for point in [upper_left, upper_right, lower_left, lower_left]:
+    for point in [upper_left, upper_right, lower_left, lower_right]:
         new_value = new_value + grid[(point[0]-1), (point[1]-1)]
 
-    grid[(midpoint[0]-1), (midpoint[1]-1)] = (new_value / 4) + random_generator()
+    grid[(midpoint[0]-1), (midpoint[1]-1)] = (new_value / 4) + random_generator(current_random_seed)
 
     return
 
-def calculate_diamondstep_values(grid, north, west, south, east, distance):
+def calculate_diamondstep_values(grid, north, west, south, east, distance, current_random_seed):
     """
     calculate the new value for the diamond points step
     """
@@ -138,7 +131,7 @@ def calculate_diamondstep_values(grid, north, west, south, east, distance):
         """
         average_value = compute_average(grid[(point1[0]-1), (point1[1]-1)], grid[(point2[0]-1), (point2[1]-1)], \
                                 grid[(point3[0]-1), (point3[1]-1)], grid[(point4[0]-1), (point4[1]-1)]) 
-        average_value = average_value + random_generator()
+        average_value = average_value + random_generator(current_random_seed)
         return float(average_value)
 
     for point in [north, west, south, east]:
@@ -148,7 +141,7 @@ def calculate_diamondstep_values(grid, north, west, south, east, distance):
 
     return
 
-def quadtree_diamond_square_algorithm(grid, nw_position, current_length):
+def quadtree_diamond_square_algorithm(grid, nw_position, current_length, current_random_seed):
     """
     in this recursive procedure the heavy lifting of calculating the square step and
     diamond step is executed, using a quadtree approach
@@ -159,17 +152,17 @@ def quadtree_diamond_square_algorithm(grid, nw_position, current_length):
         nw_point, ne_point, sw_point, se_point = find_square_step_neighbors(nw_position, current_length)
         new_length = int(current_length / 2)
         midpoint[0], midpoint[1] = calculate_midpoint(nw_point, new_length)
-        calculate_squarestep_value(grid, midpoint, nw_point, ne_point, sw_point, se_point)
+        calculate_squarestep_value(grid, midpoint, nw_point, ne_point, sw_point, se_point, current_random_seed)
         north, west, south, east = find_north_west_south_east_points(grid, midpoint, new_length)
-        calculate_diamondstep_values(grid, north, west, south, east, new_length)
+        calculate_diamondstep_values(grid, north, west, south, east, new_length, current_random_seed)
 
 
         nw_position, ne_position, sw_position, se_position = recalculate_positions(midpoint, new_length)
-
-        quadtree_diamond_square_algorithm(grid, nw_position, new_length)
-        quadtree_diamond_square_algorithm(grid, ne_position, new_length)
-        quadtree_diamond_square_algorithm(grid, sw_position, new_length)
-        quadtree_diamond_square_algorithm(grid, se_position, new_length)
+        current_random_seed = current_random_seed - 1
+        quadtree_diamond_square_algorithm(grid, nw_position, new_length, current_random_seed)
+        quadtree_diamond_square_algorithm(grid, ne_position, new_length, current_random_seed)
+        quadtree_diamond_square_algorithm(grid, sw_position, new_length, current_random_seed)
+        quadtree_diamond_square_algorithm(grid, se_position, new_length, current_random_seed)
     return
 
 #jit_module(nopython=True)
--- main_landscape.py
+++ main_landscape.py
@@ -13,20 +13,20 @@ def main():
     main procedure
     """
 
-    size = 7
+    size = 10
     last_position = [(2**size + 1), (2**size + 1)]
     first_position = [1,1]
     length = last_position[0] - first_position[0]
     current_length = length
-
-    grid = landscape.init_grid(int(last_position[0]))
+    current_random_seed = 10
+    grid = landscape.init_grid(int(last_position[0]), current_random_seed)
     start = time.time()
-    landscape.quadtree_diamond_square_algorithm(grid, first_position, current_length)
+    landscape.quadtree_diamond_square_algorithm(grid, first_position, current_length, current_random_seed)
     time_elapsed = time.time() - start
 
     print(grid)
-
-    draw_landscape.plot_landscape(grid, time_elapsed)
+    graph_type = "2D"
+    draw_landscape.plot_landscape(grid, time_elapsed, graph_type)
 
 if __name__ == "__main__":
     main()
